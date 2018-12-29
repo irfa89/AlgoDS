c09803cd9627a582b59b04ec985721641af24fef
Shariq Khan <33172597+irfa89@users.noreply.github.com>
2018-12-29 02:54:28 -0500

    Initial commit

---
+++ LinkedList_DS/App.py
@@ -0,0 +1,16 @@
+from LinkedList_DS.LinkedList import LinkedList
+
+linkedList = LinkedList()
+
+linkedList.insertEnd(12)
+linkedList.insertEnd(13)
+linkedList.insertEnd(14)
+linkedList.insertEnd(7)
+linkedList.insertEnd(9)
+linkedList.insertStart(10)
+
+linkedList.traverseList()
+
+linkedList.remove(14)
+
+linkedList.traverseList()
